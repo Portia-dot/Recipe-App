0dd94fe4329e33a028bc4ee49d0ddb3d0594545d
Modamori Oluwayomi <77457879+Portia-dot@users.noreply.github.com>
2023-11-10 17:35:04 -0800

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Oluwayomi M on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
